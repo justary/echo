f036b3ab901bb05a4f1b0af911f42a8f34977d27
Mr.justary <justary@126.com>
2021-02-05 11:29:44 +0800

    add file

---
+++ src/Carry.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Justary\Echo;
+
+class Carry{
+
+	public function desc(){
+		echo 'carry desc';
+	}
+}
---
+++ src/Tony.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Justary\Echo;
+
+class Tony{
+
+	public function desc(){
+		echo 'tony desc';
+	}
+}
